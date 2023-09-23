07c05b6e85a53d4be1d26f98216c69ebd8735bb6
yoko2001 <1766649097@qq.com>
2023-09-23 07:28:00 +0000

    first commit

---
+++ NPB3.4-OMP/mv.sh
@@ -0,0 +1,6 @@
+vmhgfs-fuse .host:/ /mnt/hgfs
+mv log/  /mnt/hgfs/sharefolders
+mv trace_record_p.txt /mnt/hgfs/sharefolders
+mv *.A.txt /mnt/hgfs/sharefolders
+mv *.B.txt /mnt/hgfs/sharefolders
+mv *stat.txt /mnt/hgfs/sharefolders
